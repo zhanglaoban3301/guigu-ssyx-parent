5c709aba038ddd826c687cc7b77885951264acc2
Administrator <282817627@qq.com>
2024-03-11 17:19:38 +0800

    3/11 gx

--- service/service-search/src/main/java/com/atguigu/ssyx/search/service/impl/SkuServiceImpl.java
+++ service/service-search/src/main/java/com/atguigu/ssyx/search/service/impl/SkuServiceImpl.java
@@ -1,5 +1,10 @@
 package com.atguigu.ssyx.search.service.impl;
 
+import com.atguigu.ssyx.client.product.ProductFeignClient;
+import com.atguigu.ssyx.enums.SkuType;
+import com.atguigu.ssyx.model.product.Category;
+import com.atguigu.ssyx.model.product.SkuInfo;
+import com.atguigu.ssyx.model.search.SkuEs;
 import com.atguigu.ssyx.search.repository.SkuRepository;
 import com.atguigu.ssyx.search.service.SkuService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,12 +14,37 @@ import org.springframework.stereotype.Service;
 public class SkuServiceImpl implements SkuService {
     @Autowired
     private SkuRepository skuRepository;
+    @Autowired
+    private ProductFeignClient productFeignClient;
     @Override
     public void upperSku(Long skuId) {
         //通过远程调用获取skuid相关信息
-
+        SkuInfo skuInfo = productFeignClient.getSkuInfo(skuId);
+        if(skuInfo == null){
+            return;
+        }
+        Category category = productFeignClient.getCategory(skuInfo.getCategoryId());
         //封装成对象
-
+        SkuEs skuEs = new SkuEs();
+        if(category != null){
+            skuEs.setCategoryId(category.getId());
+            skuEs.setCategoryName(category.getName());
+        }
+        skuEs.setId(skuInfo.getId());
+        skuEs.setKeyword(skuInfo.getSkuName()+","+skuEs.getCategoryName());
+        skuEs.setWareId(skuInfo.getWareId());
+        skuEs.setIsNewPerson(skuInfo.getIsNewPerson());
+        skuEs.setImgUrl(skuInfo.getImgUrl());
+        skuEs.setTitle(skuInfo.getSkuName());
+        if(skuInfo.getSkuType() == SkuType.COMMON.getCode()){
+            skuEs.setSkuType(0);
+            skuEs.setPrice(skuInfo.getPrice().doubleValue());
+            skuEs.setStock(skuInfo.getSale());
+            skuEs.setPerLimit(skuInfo.getPerLimit());
+        }else{
+            //TODO 待完善
+        }
+        SkuEs save = skuRepository.save(skuEs);
         //添加到es
     }
 
